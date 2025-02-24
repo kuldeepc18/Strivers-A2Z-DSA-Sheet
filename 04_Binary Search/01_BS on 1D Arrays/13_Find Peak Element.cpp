983f5448f880c4e9c300a7992ecdd42fbd1f833f
Kuldeep Choudhary <choudharykuldeep1803@gmail.com>
2025-02-24 21:40:01 +0530

    Add program to find peak element in an array

--- 04_Binary Search/01_BS on 1D Arrays/13_Find Peak Element.cpp
+++ 04_Binary Search/01_BS on 1D Arrays/13_Find Peak Element.cpp
@@ -6,30 +6,43 @@ using namespace std;
 // Optimal Approach - Binary Search 
 // Time Complexity - O(logN)
 // Space Complexity - O(1)
-int findPeakElement(vector<int> &arr) {
-    int n = arr.size(); //Size of array.
+int findPeakElement(vector<int>& arr) {
 
-    // Edge cases:
-    if (n == 1) return 0;
-    if (arr[0] > arr[1]) return 0;
-    if (arr[n - 1] > arr[n - 2]) return n - 1;
+    int n = arr.size();
+    // Edge cases
+    if (n == 1)
+        return 0;
+    if (arr[0] > arr[1])
+        return 0;
+    if (arr[n - 1] > arr[n - 2])
+        return n - 1;
 
     int low = 1, high = n - 2;
     while (low <= high) {
         int mid = (low + high) / 2;
 
-        //If arr[mid] is the peak:
-        if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
+        // if arr[mid] is peak
+        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
             return mid;
+        }
 
-        // If we are in the left:
-        if (arr[mid] > arr[mid - 1]) low = mid + 1;
+        // mid is on increasing curve and peak is on right of it.
+        else if (arr[mid] > arr[mid - 1]) {
+            low = mid + 1;
+        }
 
-        // If we are in the right:
-        // Or, arr[mid] is a common point:
-        else high = mid - 1;
+        // mid is on decreasing curve and peak is on left of it.
+        else if (arr[mid] > arr[mid + 1]) {
+            high = mid - 1;
+        }
+
+        // if mid is between both increasing or decreasing curve then move
+        // to any of the side
+        else {
+            low = mid + 1;
+        }
     }
-    // Dummy return statement
+
     return -1;
 }
 
