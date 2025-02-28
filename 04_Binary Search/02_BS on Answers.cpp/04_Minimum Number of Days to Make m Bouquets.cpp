0dfed50d74cf7c6d0a426c03f9f62814d0eea9d4
Kuldeep Choudhary <choudharykuldeep1803@gmail.com>
2025-02-28 22:11:37 +0530

    Add program to find minimum no. of days to makae m bouquets

--- 04_Binary Search/02_BS on Answers.cpp/04_Minimum Number of Days to Make m Bouquets.cpp
+++ 04_Binary Search/02_BS on Answers.cpp/04_Minimum Number of Days to Make m Bouquets.cpp
@@ -3,7 +3,47 @@ using namespace std;
 
 
 
-// Optimal Appraoch - Binary Search
+// Brute force
+// Time Complexity: O((max(arr[])-min(arr[])+1) * N), where {max(arr[]) -> maximum element of the array, min(arr[]) -> minimum element of the array, N = size of the array}.
+// Space Complexity: O(1)
+// bool possible(vector<int> &arr, int day, int m, int k) {
+//     int n = arr.size(); //size of the array
+//     int cnt = 0;
+//     int noOfB = 0;
+//     // count the number of bouquets:
+//     for (int i = 0; i < n; i++) {
+//         if (arr[i] <= day) {
+//             cnt++;
+//         }
+//         else {
+//             noOfB += (cnt / k);
+//             cnt = 0;
+//         }
+//     }
+//     noOfB += (cnt / k);
+//     return noOfB >= m;
+// }
+// int roseGarden(vector<int> arr, int k, int m) {
+//     long long val = m * 1ll * k * 1ll;
+//     int n = arr.size(); //size of the array
+//     if (val > n) return -1; //impossible case.
+//     //find maximum and minimum:
+//     int mini = INT_MAX, maxi = INT_MIN;
+//     for (int i = 0; i < n; i++) {
+//         mini = min(mini, arr[i]);
+//         maxi = max(maxi, arr[i]);
+//     }
+
+//     for (int i = mini; i <= maxi; i++) {
+//         if (possible(arr, i, m, k))
+//             return i;
+//     }
+//     return -1;
+// }
+
+
+
+// Optimal Approach - Binary Search
 // Time Complexity: O(log(max(arr[])-min(arr[])+1) * N), where {max(arr[]) -> maximum element of the array, min(arr[]) -> minimum element of the array, N = size of the array}.
 // Space Complexity: O(1)
 bool possible(vector<int> &arr, int day, int m, int k)
